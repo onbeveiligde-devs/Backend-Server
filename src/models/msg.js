2efbb5d52bbf3447100b8dbc1e2d41e08c724c48
noobping <noobping@users.noreply.github.com>
2019-06-19 10:55:58 +0200

    remove auth endpoint, restore mony controller

--- src/models/msg.js
+++ src/models/msg.js
@@ -5,48 +5,52 @@ const {
 const Chat = require('../models/db/Chat');
 
 module.exports = {
-    /**
-     * @param msg = the chat message (unencrypted)
-     * @param hash = the hash of the message. It is encrypted with the private key from the author
-     * @param certificate = the certificate of the message author
-     */
-    save(msg, hash, certificate, done) {
+    save(data, done) {
         const chat = new Chat({
-            certificateSubject: certificate,
-            certificateAuthor: null,
-            messageHash: hash,
-            message: msg
+            certificateSubject: data.certificateSubject,
+            certificateAuthor: data.certificateAuthor,
+            messageHash: data.messageHash,
+            message: data.message
         });
         chat.save()
             .then((reply) => {
-                let resource = new Resource({
-                    created: !chat.isNew,
-                    data: reply._doc
-                }, req.url);
-
-                let str = req.url;
-                if (str.substr(-1) != '/') str += '/';
-                str += chat._id;
-                resource.link(chat._id, str);
-
-                res.send(resource);
+                done({
+                    ok: !chat.isNew,
+                    message: 'message saved',
+                    data: reply
+                });
             })
             .catch(err => {
                 console.log('can not create chat. ', err);
-                res.status(200);
-                res.send(new Hal.Resource({
-                    message: 'can not create chat.',
-                    errors: err
-                }, req.url));
+                done({
+                    ok: false,
+                    message: 'can not create chat',
+                    data: err
+                });
             });
         done((true));
     },
 
     /** 
-     * @param certificate = the certificate of the transparent person
+     * @param subject = the certificate of the transparent person
      * @returns Array with the history of messages
      */
-    load(certificate, done) {
-        done([]);
+    load(subject, done) {
+        Chat.find({
+            certificateSubject: subject
+        }).then(messages => {
+                done({
+                    ok: true,
+                    data: messages
+                });
+            })
+            .catch(err => {
+                console.log('can not get a list of messages. ', err);
+                done({
+                    ok: false,
+                    message: 'can not get a list of messages.',
+                    data: err
+                });
+            });
     }
 }
