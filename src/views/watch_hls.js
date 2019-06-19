f6a936186037df70558f8dcaf959a7dfb9848d86
Arjan Gosens <arjangosens@hotmail.com>
2019-06-19 11:33:47 +0200

    Added manifest loading to bindVideo function
    Does not work due to it missing the required source, which will be added soon.

--- src/views/watch_hls.js
+++ src/views/watch_hls.js
@@ -3,11 +3,17 @@ function bindVideo() {
 
     if (Hls.isSupported()) {
         var hls = new Hls();
-
         hls.attachMedia(video);
+
         hls.on(Hls.Events.MEDIA_ATTACHED, function () {
             console.log("video and hls.js are now bound together!")
-        })
+            hls.loadSource("SOURCE_HERE"); //SOURCE_HERE needs to be replaced with actual source
+
+            hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+                console.log("Manifest loaded, found " + data.levels.length + " quality level");
+            });
+        });
+
     } else {
         console.log("ERROR: Could not bind video and hls.js!")
     }
