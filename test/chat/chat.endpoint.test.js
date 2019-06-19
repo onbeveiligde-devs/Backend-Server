ba9b65c072251593abef056443865df8f6dd3253
Rowan Dorst <rdorst1@avans.nl>
2019-06-19 12:01:27 +0200

    Start of chat endpoint test

--- test/chat/chat.endpoint.test.js
+++ test/chat/chat.endpoint.test.js
@@ -4,3 +4,17 @@ const {
     app
 } = require('../../app');
 const Chat = require('../../src/models/db/Chat');
+
+describe('Chat endpoint test', () => {
+
+    it.only('Get request returns list of chat messages', (done) => {
+        request(app)
+            .get('/chat')
+            .expect(200)
+            .end((err, res) => {
+                console.log(res.body)
+                assert(Array.isArray(res.body.messages))
+                done();
+            })
+    })
+})
