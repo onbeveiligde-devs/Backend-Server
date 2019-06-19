9dd5cdeceff53c20b2d0ff583bbf2fc6b50c537e
Arjan Gosens <arjangosens@hotmail.com>
2019-06-19 12:36:04 +0200

    Added test source from hls github page, video now starts playing on page load

--- src/views/watch_hls.js
+++ src/views/watch_hls.js
@@ -1,18 +1,21 @@
 function bindVideo() {
-    var video = document.getElementById('video');
 
     if (Hls.isSupported()) {
+        var video = document.getElementById('video');
         var hls = new Hls();
+
         hls.attachMedia(video);
 
         hls.on(Hls.Events.MEDIA_ATTACHED, function () {
             console.log("video and hls.js are now bound together!")
-            hls.loadSource("SOURCE_HERE"); //SOURCE_HERE needs to be replaced with actual source
+            hls.loadSource("https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8"); //SOURCE_HERE needs to be replaced with actual source
 
             hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
                 console.log("Manifest loaded, found " + data.levels.length + " quality level");
             });
         });
+
+        video.play();
 
     } else {
         console.log("ERROR: Could not bind video and hls.js!")
