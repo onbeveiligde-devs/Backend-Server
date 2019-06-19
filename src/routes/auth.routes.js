24f3f2c1ce533b53b3b37de8135e3f44c4ae0cbe
noobping <noobping@users.noreply.github.com>
2019-06-19 21:01:41 +0200

    updated startpage

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const controller = require('../controllers/auth.controller');
-
-module.exports = (app) => {
-    app.post('/private', controller.private);
-    app.post('/public', controller.public);
-    app.post('/hash', controller.hash);
-    app.post('/verify', controller.verify);
-};
