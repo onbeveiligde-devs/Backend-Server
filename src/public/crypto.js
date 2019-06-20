15ceedda456d7f440423259e1027b6e67977e38f
Jantje2000 <jan@vanderstar.re>
2019-06-20 11:30:50 +0200

    Removed the integrity

--- src/public/crypto.js
+++ src/public/crypto.js
@@ -1,16 +1,22 @@
 function sign(data) {
 
-    const exportedPrivateKey = JSON.parse(localStorage.getItem("exportedPrivateKey"));
+    const exportedPrivateKey = localStorage.getItem("exportedPrivateKey");
 
     console.log("exportedPrivateKey");
     console.log(exportedPrivateKey);
 
+    console.log("str2ab(exportedPrivateKey)");
+    console.log(str2ab(exportedPrivateKey));
+
     return crypto.subtle.importKey(
-        "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-        exportedPrivateKey,
-        {   //these are the algorithm options
-            name: "ECDSA",
-            namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
+        "pkcs8", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+        str2ab(exportedPrivateKey),
+        {
+            name: "RSASSA-PKCS1-v1_5",
+            // Consider using a 4096-bit key for systems that require long-term security
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: "SHA-256",
         },
         true, //whether the key is extractable (i.e. can be used in exportKey)
         ["sign"] //"verify" for public key import, "sign" for private key imports
@@ -18,12 +24,14 @@ function sign(data) {
         //returns a publicKey (or privateKey if you are importing a private key)
         return crypto.subtle.sign(
             {
-                name: "ECDSA",
+                name: "RSASSA-PKCS1-v1_5",
                 hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
             },
             privateKey, //from generateKey or importKey above
-            str2ab(btoa(JSON.stringify(data))) //ArrayBuffer of data you want to sign
+            str2ab("Hoi") //ArrayBuffer of data you want to sign
         )
+        // str2ab(btoa(JSON.stringify(data))) //ArrayBuffer of data you want to sign
+
     });
 }
 
