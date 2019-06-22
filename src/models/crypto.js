a847f5935cabf6415343b7fd8035fa59c9c50b67
teunovic <ttfcolin@avans.nl>
2019-06-22 23:29:59 +0200

    Made the crypto class compatible with browser, because looking at my example client code would be sooo hard. Also added example code for the crypto class for nick.

--- src/models/crypto.js
+++ src/models/crypto.js
@@ -1,13 +1,19 @@
-const WebCrypto = require("@trust/webcrypto");
-const atob = require('atob');
-const btoa = require('btoa');
-const Base64ArrayBufferUtil = require('base64-arraybuffer');
+const WebCrypto = require("@trust/webcrypto"); // BROWSER VERSION -> const WebCrypto = window.crypto.subtle;
+const atob = require('atob'); // Remove if on browser
+const btoa = require('btoa'); // Remove if on browser
+
+
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+let lookup = new Uint8Array(256);
+
+for (let i = 0; i < chars.length; i++) {
+    lookup[chars.charCodeAt(i)] = i;
+}
 
 module.exports = {
-    
-    verify: async function(data, sign, publicKeyBase64) { // data should be a string, sign should be a base64sign (string)
+    verify: function(data, sign, publicKeyWrappedBase64) { // data should be a string, sign should be a base64sign (string)
         return new Promise(async (res, rej) => {
-            let pub = await this.unwrapKey(publicKeyBase64, false);
+            let pub = await this.unwrapKey(publicKeyWrappedBase64, false);
 
             WebCrypto.subtle.verify(
                 {
@@ -15,7 +21,7 @@ module.exports = {
                     hash: {name: "SHA-256"},
                 },
                 pub,
-                Base64ArrayBufferUtil.decode(sign),
+                this.b642ab(sign),
                 this.str2ab(data)
             )
                 .then(succ => {
@@ -23,7 +29,7 @@ module.exports = {
                 })
                 .catch(err => {
                     console.error(err);
-                    rej(err);
+                    rej(err => rej(err));
                 });
         });
     },
@@ -32,7 +38,7 @@ module.exports = {
         return new Promise((res, rej) => {
             WebCrypto.subtle.importKey(
                 'jwk',
-                atob(JSON.parse(key)),
+                JSON.parse(atob(key)),
                 {
                     name: "RSASSA-PKCS1-v1_5",
                     hash: {name: "SHA-256"},
@@ -48,20 +54,25 @@ module.exports = {
             WebCrypto.subtle.exportKey(
                 'jwk',
                 key)
-                .then(jwk => btoa(JSON.stringify(jwk)))
-                .catch(rej);
+                .then(jwk => res(btoa(JSON.stringify(jwk))))
+                .catch(err => rej(err));
         });
     },
 
-    // Data must be a string, privateKey must be a CryptoKey (UNWRAPPED)
-    sign: function(data, privateKey) {
+    // Data must be a string, privateKey must be a CryptoKey (UNWRAPPED, in object form)
+    sign: function(data, privateKeyCryptoKey) {
         return new Promise((res, rej) => {
-            WebCrypto.subtle.sign({
-                    name: "RSASSA-PKCS1-v1_5"
+            WebCrypto.subtle.sign(
+                {
+                    name: "RSASSA-PKCS1-v1_5",
+                    hash: {name: "SHA-256"},
                 },
-                privateKey,
-                str2ab(data))
-                .then(signature => res(signature))
+                privateKeyCryptoKey,
+                this.str2ab(data)
+            )
+                .then(signature => {
+                    res(this.ab2b64(signature));
+                })
                 .catch(err => rej(err));
         });
     },
@@ -72,8 +83,8 @@ module.exports = {
     //      publicKey   (CryptoKey, so it is UNWRAPPED!!!)
     // }
     generateKeyPair: function() {
-        return new Promise<CryptoKeyPair>((res, rej) => {
-            window.crypto.subtle.generateKey(
+        return new Promise((res, rej) => {
+            WebCrypto.subtle.generateKey(
                 {
                     name: "RSASSA-PKCS1-v1_5",
                     modulusLength: 2048, //can be 1024, 2048, or 4096
@@ -83,7 +94,7 @@ module.exports = {
                 true, //whether the key is extractable (i.e. can be used in exportKey)
                 ["sign", "verify"]) //can be any combination of "sign" and "verify")
                 .then(keyPair => res(keyPair))
-                .catch(console.error)
+                .catch(err => rej(err))
         });
     },
 
@@ -98,6 +109,57 @@ module.exports = {
             bufView[i] = str.charCodeAt(i);
         }
         return buf;
+    },
+
+    // ArrayBuffer to Base64 string
+    ab2b64(arraybuffer) {
+        let bytes = new Uint8Array(arraybuffer),
+            i, len = bytes.length, base64 = "";
+
+        for (i = 0; i < len; i+=3) {
+            base64 += chars[bytes[i] >> 2];
+            base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+            base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+            base64 += chars[bytes[i + 2] & 63];
+        }
+
+        if ((len % 3) === 2) {
+            base64 = base64.substring(0, base64.length - 1) + "=";
+        } else if (len % 3 === 1) {
+            base64 = base64.substring(0, base64.length - 2) + "==";
+        }
+
+        return base64;
+    },
+
+    // Base64 string to ArrayBuffer
+    b642ab(base64) {
+        let bufferLength = base64.length * 0.75,
+            len = base64.length, i, p = 0,
+            encoded1, encoded2, encoded3, encoded4;
+
+        if (base64[base64.length - 1] === "=") {
+            bufferLength--;
+            if (base64[base64.length - 2] === "=") {
+                bufferLength--;
+            }
+        }
+
+        let arraybuffer = new ArrayBuffer(bufferLength),
+            bytes = new Uint8Array(arraybuffer);
+
+        for (i = 0; i < len; i+=4) {
+            encoded1 = lookup[base64.charCodeAt(i)];
+            encoded2 = lookup[base64.charCodeAt(i+1)];
+            encoded3 = lookup[base64.charCodeAt(i+2)];
+            encoded4 = lookup[base64.charCodeAt(i+3)];
+
+            bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
+            bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+            bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+        }
+
+        return arraybuffer;
     }
 
 };
