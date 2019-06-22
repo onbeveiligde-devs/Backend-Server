a0a440c5ef8a92a29848354c6308be652cde5517
teunovic <ttfcolin@avans.nl>
2019-06-22 23:32:27 +0200

    It's beyond bizarre to host plain server code, but here's a fix 8-)

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cross()); // Allow Cross Domain Requests
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views');
 app.use(express.static(path.join(__dirname, 'src/public')));
-app.use('/crypto', express.static(path.join(__dirname, 'src/controllers/crypto.js')));
+app.use('/crypto', express.static(path.join(__dirname, 'src/models/crypto.js')));
 
 // endpoints
 const streamRoutes = require('./src/routes/stream.routes');
